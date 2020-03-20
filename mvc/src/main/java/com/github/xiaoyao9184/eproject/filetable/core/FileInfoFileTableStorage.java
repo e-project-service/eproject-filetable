7031e11c3f90abbdb2cc368dc914fbfc197168ec
xiaoyao9184 <xiaoyao9184@gmail.com>
2020-03-20 16:08:51 +0800

    fix missing FileInfoFileTableStorage

---
+++ mvc/src/main/java/com/github/xiaoyao9184/eproject/filetable/core/FileInfoFileTableStorage.java
@@ -0,0 +1,11 @@
+package com.github.xiaoyao9184.eproject.filetable.core;
+
+import com.github.xiaoyao9184.eproject.filetable.core.filestorage.FileTableConvertibleStorage;
+import com.github.xiaoyao9184.eproject.filetable.model.FileInfo;
+
+/**
+ * Created by xy on 2020/3/20.
+ */
+public class FileInfoFileTableStorage extends FileTableConvertibleStorage<FileInfo> {
+
+}
